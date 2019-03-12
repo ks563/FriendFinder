adf93d30461f5fa49e154a8e2c065132ca0e495d
ks563 <karen.shea90@gmail.com>
2019-03-11 19:33:47 -0500

    post written

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,14 +9,21 @@ module.exports = function (app) {
 
     app.post("/api/friends", function (req, res) {
         console.log(req.body);
-        for()
+        console.log(friendsData);
         var newPerson = req.body;
-
-
-        //needs compatability logic
-        // if () {
-            //if the match score is equivalent to  certain score then the people will be a match
-        // }
+        var bestChoice = 50;
+        var currentChoice;
+        var friendMath;
+        for (var i = 0; i > friendsData.length; i++){
+            for (var j = 0; j > newPerson.score.length; j++){
+              friendMath += Math.abs(friendsData[i].score[j] - newPerson.score[j])
+            }
+            if (friendMath < bestChoice) {
+                currentChoice = friendData[i];
+            }
+            friendMath = 0; 
+        }
+        
     })
 
 }
